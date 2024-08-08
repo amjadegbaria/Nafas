83d4901055b86a34515b9993faba0f38bbf04768
akhalaily <akhalaily@paloaltonetworks.com>
2024-08-08 13:54:55 +0300

    classes was refactored, now need to adopt the flow - check Flo3.py

---
+++ classes/Flow.py
@@ -0,0 +1,30 @@
+class Flow:
+    def __init__(self, questions: dict):
+        """
+        Initializes the flow with a dictionary of questions.
+        The dictionary keys are question IDs and values are Question objects.
+        """
+        self.questions = questions
+        self.current_index = -1
+        self.history = []
+
+    def get_current_question(self):
+        if self.current_index >= 0:
+            current_id = self.history[self.current_index]
+            return self.questions.get(current_id)
+        return None
+
+    def move_to_next_question(self, next_question_id: str):
+        if next_question_id:
+            self.history.append(next_question_id)
+            self.current_index += 1
+            return self.questions.get(next_question_id)
+        return None
+
+
+    def start_flow(self, start_question_id: str):
+        """
+        Starts the flow from a specific question.
+        """
+        self.history = [start_question_id]
+        self.current_index = 0
