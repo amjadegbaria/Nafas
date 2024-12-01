979a5b8a31bb028cd69ce8af1044ff3c6f931eb9
akhalaily <akhalaily@paloaltonetworks.com>
2024-12-01 14:56:43 +0200

    Adding restart flow, when 15 min has been passed

--- handlers/command_handler.py
+++ handlers/command_handler.py
@@ -1,8 +1,9 @@
 from telegram import Update
-from telegram.ext import  CallbackContext, ContextTypes
-# from flows.Flow3 import flow
-from handlers.message_handler import handle_media, handle_message, answered_questions
-from flows.flow_handler import start_flow, get_user_flow
+from telegram.ext import CallbackContext, ContextTypes
+from handlers.message_handler import handle_media, handle_message, answered_questions, trigger_restart_flow
+from flows.flow_handler import start_flow, get_user_flow, check_user_last_interaction
+from database.queries import get_user_data, reset_user_progress
+
 
 async def start(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
@@ -10,7 +11,7 @@ async def start(update: Update, context: CallbackContext) -> None:
     flow = get_user_flow(user_id)
     question = flow.get_current_question()
     flow.start_flow(question.get_id())  # Start with the first question
-    start_flow(user_id, flow.get_flow_id(), question.get_id())
+    start_flow(user_id, flow.get_id(), question.get_id())
     await handle_media(question, update, context)
 
 
@@ -24,10 +25,19 @@ async def restart(update: Update, context: CallbackContext) -> None:
 
 async def default(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.message.from_user.id
+    user_data = get_user_data(user_id)
     flow = get_user_flow(user_id)
-    if not answered_questions.get(user_id, None):  # flow did not start yet
+
+    if check_user_last_interaction(user_data):
+        await trigger_restart_flow(update, context)
+    elif not answered_questions.get(user_id, None):  # flow did not start yet
         await start(update, context)
     elif flow.is_completed():  # if the user reaches the last question, restart
         await restart(update, context)
     else:
         await handle_message(update, context)
+
+async def restart_flow(update, context):
+    user_id = update.effective_user.id
+    reset_user_progress(user_id)  # clean the restart flow from the DB
+    await start(update, context)
